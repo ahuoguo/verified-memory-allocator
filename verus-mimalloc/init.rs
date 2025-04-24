2352ae7972f3d1e12b36989ba6ffb12bc6670367
Alexander Bai <abai@pinky09.mpi-sws.org>
2025-04-24 18:09:13 +0200

    failing example

--- verus-mimalloc/init.rs
+++ verus-mimalloc/init.rs
@@ -24,10 +24,10 @@ use crate::os_mem::*;
 use crate::thread::*;
 
 verus!{
-    broadcast use vstd::seq_lib::group_seq_properties,
-    vstd::map_lib::group_map_properties,
-    vstd::set_lib::group_set_properties,
-    vstd::multiset::group_multiset_properties;
+//    broadcast use vstd::seq_lib::group_seq_properties,
+//    vstd::map_lib::group_map_properties,
+//    vstd::set_lib::group_set_properties,
+//    vstd::multiset::group_multiset_properties;
 
 pub tracked struct Global {
     pub(crate) tracked instance: Mim::Instance,
@@ -725,7 +725,7 @@ fn init_empty_page_ptr() -> (e: EmptyPageStuff)
 
 //    assert(set_int_range(pt as int, pt as int + 4096) <= mc.range_os_rw());
 //    assert(set_int_range(pt as int, pt as int + 4096) <= mc.range_points_to());
-//    assert(mc.pointsto_has_range(pt as int, 4096));
+      assert(mc.pointsto_has_range(pt as int, 4096));
 //    assert(mc.pointsto_has_range(pt as int, SIZEOF_PAGE_HEADER as int));
     let tracked points_to_raw = mc.take_points_to_range(pt as int, SIZEOF_PAGE_HEADER as int);
     proof {
